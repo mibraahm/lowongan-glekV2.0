49be4be3f96f82f45517e35d8f26437d4e288f04
mibraahm <mochibra25@gmail.com>
2022-08-08 13:04:16 +0700

    Upload file

---
+++ script.js
@@ -0,0 +1,155 @@
+document.querySelector(".sales").addEventListener("click", function () {
+  Swal.fire({
+    title: "Sales Manager",
+    html: "<br><b>JOB DUTIES</b><br><br>• Identifying, designing and implementing business plans and strategies to promote business growth<br>• Organizing, or re-organizing, workflows to improve coordination between all business parties and maximize efficiency and productivity.<br>• Performing regular evaluations to identify areas of improvement<br>• Reporting for CFO & CEO<br><br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in timely manner<br>• Tartil in reciting the Al-quran<br>• No smoking and no tattoos<br>• Male 28-40 years old<br>• Familiar working with Google Workspace<br>• Minimum 5 years of sales experience at the same level<br>• Bachelor’s degree in business, business management or related field<br>• Excellent organizational and time management skills<br>• Excellent written and verbal communication skills<br>• Demonstrable leadership abilities and excellent interpersonal skills<br>• Professional knowledge of the latest business policies and regulations<br>• Possess business or industry insights with demonstrable analytical skills <br>• Problem-solving skills",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/KRNttfxSKPtDZnkg6' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".hrm").addEventListener("click", function () {
+  Swal.fire({
+    title: "HR MANAGER",
+    html: "<br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 28-40 years old<br>• Familiar working with Google Workspace<br>• Minimum 5 years experience in HR field<br>• Independent personality, communicative, humble, active<br>• Able to speak English fluently<br>• Adaptable<br>• Advanced IT skills (Word processing, Sheets, Databases, Internet)<br>• Ability to facilitate discussion with individuals, and groups",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/whavVNc6ojSZqYfJ9' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".prm").addEventListener("click", function () {
+  Swal.fire({
+    title: "Procurement Manager",
+    html: "<br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 28-40 years old<br>• Familiar working with Google Workspace<br>• 2+ years of experience as a procurement officer or in a similar position in a technology startup<br>• Good analytical and strategic thinking skills<br>• Excellent written and verbal communication skills<br>• Certified Procurement Professional is a plus<br>• No Smoking and No Tattoos",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/PfWGPTCndfWudhj56' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".dmc").addEventListener("click", function () {
+  Swal.fire({
+    title: "DIGITAL MARKETING COORDINATOR",
+    html: "<br><b>JOB DUTIES</b><br><br>• Research advertising trends<br>• Research competitors’ pricing and product’s campaign<br>• Decide on appropriate placement of ads<br>• Determine what content will reach customers<br>• Develop projects to create content<br>• Publish digital marketing content online<br>• Implement email marketing campaigns<br>• Measure digital traffic<br>• Monitor social media and Google Analytics<br>• Optimize paid advertising campaigns using SEO and other tools<br>• Report on the growth and analytics of campaigns to stakeholders<br>• Monitor project status and budget<br>• Conduct market research to inform campaigns<br>• Brainstorm and implement experiments and conversion tests<br><br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 28-40 years old<br>• Familiar working with Google Workspace<br>• Minimum 3 years experience at the same level<br>• Bachelor’s degree in communications or related field<br>• Excellent organizational and time management skills<br>• Excellent written and verbal communication skills<br>• Demonstrable leadership abilities and excellent interpersonal skills<br>• Professional knowledge of the latest business policies and regulations",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/eguB5yAQPx773JWo7' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".vde").addEventListener("click", function () {
+  Swal.fire({
+    title: "Video Editor",
+    html: "<br><b>JOB DUTIES</b><br><br>• We are looking for a talented Video Editor to assemble recorded footage into a finished project that is suitable for broadcasting<br>• Ultimately should be able to bring sight and sound together in order to tell a cohesive story<br>• Creative mind and storytelling skills<br>• Manipulate and edit film pieces in a way that is invisible to the audience<br>• Take a brief to grasp production team’s needs and specifications<br>• Review shooting script and raw material to create a shot decision list based on scenes’ value and contribution to continuity<br>• Trim footage segments and put together the sequence of the film<br>• Input audio, dialogues, graphics and effects<br>• Create rough and final cuts<br>• Ensure logical sequencing and smooth running<br>• Consult with stakeholders from production to post-production process<br><br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 20-35 years old<br>• Familiar working with Google Workspace<br>• Proven work experience as Video Editor<br>• Bachelor’s degree in Film or Cinematography or related field<br>• Excellent time management skills<br>• Excellent written and verbal communication skills<br>• Excellent interpersonal skills<br>• Problem-solving skills<br>• Solid experience with digital technology and open-source editing software ",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/T1X6sur4ptKEZDfR6' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".aep1").addEventListener("click", function () {
+  Swal.fire({
+    title: "Account Executive & Partnership Regional 1 (Jabodetabek)",
+    html: "<br><b>JOB DUTIES</b><br><br>• Expanding the current network and identify prospective clients<br>• Handing all processes within the sales cycle: from pitching to closing<br>• Presenting products or services to clients<br>• Maintaining close contact with clients and attending regular meetings<br>• Providing after-sales support to clients: invoicing, preparing reports, handling complaints and resolving any issues that arise<br>• Ultimately should be able to bring sight and sound together in order to tell a cohesive story<br><br><b>QUALIFICATION</b><br><br>• Moslem<br>• Live around Jabodetabek<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 20-35 years old<br>• Familiar working with Google Workspace<br>• Proven work experience at the same position<br>• Bachelor’s degree in Business Management or related field<br>• Excellent time management skills<br>• Excellent written and verbal communication skills<br>• Excellent interpersonal skills<br>• Problem-solving skills",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/N7TQ1e6mQNE5Eqnq6' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".aep2").addEventListener("click", function () {
+  Swal.fire({
+    title: "Account Executive & Partnership Regional 1 (Surabaya)",
+    html: "<br><b>JOB DUTIES</b><br><br>• Expanding the current network and identify prospective clients<br>• Handing all processes within the sales cycle: from pitching to closing<br>• Presenting products or services to clients<br>• Maintaining close contact with clients and attending regular meetings<br>• Providing after-sales support to clients: invoicing, preparing reports, handling complaints and resolving any issues that arise<br>• Ultimately should be able to bring sight and sound together in order to tell a cohesive story<br><br><b>QUALIFICATION</b><br><br>• Moslem<br>• Live around Surabaya<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 20-35 years old<br>• Familiar working with Google Workspace<br>• Proven work experience at the same position<br>• Bachelor’s degree in Business Management or related field<br>• Excellent time management skills<br>• Excellent written and verbal communication skills<br>• Excellent interpersonal skills<br>• Problem-solving skills",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/5HHzhZ7rgnGjLKS27' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".rnd").addEventListener("click", function () {
+  Swal.fire({
+    title: "Research & Development Coffee Beverages (Experienced Barista)",
+    html: "<br><b>JOB DUTIES</b><br><br>• Responsible to do research & development for beverages product recipe formulation and packaging<br>• Create new product to sell to outlets and other market segment<br>• Finding the good taste of beverage which acceptable in market's palate<br><br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Male 20-35 years old<br>• Familiar working with Google Workspace<br>• Proven work experience as RND Beverages<br>• Bachelor’s degree in Hospitality or related field<br>• Excellent time management skills<br>• Excellent written and verbal communication skills<br>• Excellent interpersonal skills<br>• Problem-solving skills",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/16fpag37aWJSDcVD6' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".ito").addEventListener("click", function () {
+  Swal.fire({
+    title: "IT OPERATION",
+    html: "<br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Have at least a Diploma (D3), but preferably a Bachelor's Degree (S1)<br>• Experienced in the field of IT Support<br>• Proficient in SQL, HTML, and VB programming languages<br>• Able to communicate effectively, both written and verbal<br>• Mastering Microsoft Office, especially Microsoft Excel (VLOOKUP/HLOOKUP, SUMIF, IF, PivotTable, etc.)<br>• A problem solver with a keen eye for detail, a desire to learn, and the ability to work flexible hours<br>• No Smoking and No Tattoos",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/DWCPrQ7Dpdm22Tdx6' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".mc").addEventListener("click", function () {
+  Swal.fire({
+    title: "Manager Compliance",
+    html: "<br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• Bachelor of Laws with a minimum of 3-5 years of professional experience<br>• Able to speak English<br>• Excellent negotiation, drafting and analysis skills<br>• Proactive and can be a team player<br>• Have the ability to work in a time sensitive environment and ensure deadlines are met on time<br>• A problem solver with a keen eye for detail, a desire to learn, and the ability to work flexible hours<br>• No Smoking and No Tattoos",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/3nK7v6WsSUmLmUzy8' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".ml").addEventListener("click", function () {
+  Swal.fire({
+    title: "Manager Legal",
+    html: "<br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• At least 5 years experience in the field of law<br>• Proficiency level of spoken and written English<br>• Team player and ambitious<br>• Highly responsible, motivated, independent and passionate about the law<br>• Have the ability to adapt and have a strong will to work in a fast-paced team environment",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/uUA4TzV1HHVziq1S9' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
+
+document.querySelector(".opm").addEventListener("click", function () {
+  Swal.fire({
+    title: "OPERATIONS MANAGER",
+    html: "<br><b>QUALIFICATION</b><br><br>• Moslem<br>• Shalat in a timely manner<br>•Tartil in reciting the Al-quran <br>• No smoking and no tattoos<br>• At least bachelor in any major<br>• Critical and structured thinking<br>• Minimum 1 year experience, mastering MS Excel is a bonus value<br>• Excellent leadership, project management, communication, analytical and organizational skills<br>• Willing to learn, share, and collaborate with other team members",
+    showCancelButton: true,
+    confirmButtonText: "<a target='_blank' href='https://forms.gle/rHZkEjYWM6eFAgWg7' style='color:white; text-decoration:none'>Interested</a>",
+    cancelButtonText: "Back",
+    confirmButtonColor: "#3ad2c3",
+    cancelButtonColor: "#999",
+    reverseButtons: true,
+  });
+});
